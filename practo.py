3de6ff5a14775dd538b7b66b29d3225223fdd03d
VH Praneeth <48318416+vhpraneeth@users.noreply.github.com>
2020-08-20 19:38:25 +0530

    Update practo.py

--- practo.py
+++ practo.py
@@ -6,26 +6,29 @@ from datetime import datetime
 import csv
 from lxml import html
 from bs4 import BeautifulSoup
+import pandas as pd
 from selenium import webdriver
 
 os.system('mkdir csv_practo > /dev/null 2>&1')
-options = webdriver.ChromeOptions()
-options.add_argument('headless')
-driver = webdriver.Chrome(options=options)
+# options = webdriver.ChromeOptions()  #
+# options.add_argument('headless')  #
+# driver = webdriver.Chrome(options=options)
 
 print("Starting...")
 done = []
 url_format = 'https://www.practo.com/tests?city={}'
 category_url_format = 'https://www.practo.com/health-checkup-packages/{}?city={}'  # category, city
-all_cities = ['delhi', 'mumbai', 'hyderabad', 'bangalore', 'kolkata']
-all_categories = ['diabetes-checkup', 'cancer-screening-health-checkup', 'skin-care-checkups', 'kidney-urine-checkups', 
+all_cities = ['delhi', 'mumbai']  # , 'hyderabad', 'bangalore', 'kolkata']
+all_categories = ['diabetes-checkup', 'cancer-screening-health-checkup', 'skin-care-checkups', 'kidney-urine-checkups',
                   'stomach-digestion-checkups', 'sexual-wellness-checkups', 'bone-joints-checkups', 'fever-checkup']
+test_columns = ['Test name', 'Alternate name', 'Price', 'What is this test?', 'Why this test is performed?',
+                'Frequency', 'Precautions', 'Test Preparation', 'Understanding your test results', 'Link']
 
 
 def process_tests_list(tests_list, filename):
     global done
     for url in tests_list:
-        '''while 1:
+        while 1:
             try:
                 res = requests.get(url)
                 res.raise_for_status()
@@ -36,46 +39,37 @@ def process_tests_list(tests_list, filename):
                 print('URL:', url)
                 break  # return  # pending
             else:
-                break'''
-        driver.get(url)
-        tree = html.fromstring(driver.page_source)  # res.text
+                break
+        # driver.get(url)
+        tree = html.fromstring(res.text)  # driver.page_source
         name = '/html/body/div[1]/div/div/div[2]/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[1]/h1/text()'
         alt_name = '/html/body/div[1]/div/div/div[2]/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[1]/span/span[2]/text()'
         price = '/html/body/div[1]/div/div/div[2]/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[2]/div[2]/div[1]/div[1]/text()'
-        what_is_this_test = '/html/body/div[1]/div/div/div[2]/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[4]/div/div/div/div/p[1]/text()'
-        what_is_this_test_2 = '/html/body/div[1]/div/div/div[2]/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[4]/div/div/div/div/p[2]/text()'
-        why_performed = '/html/body/div[1]/div/div/div[2]/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[4]/div/div/div/div/p[3]/text()'
-        frequency = '/html/body/div[1]/div/div/div[2]/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[4]/div/div/div/div/p[4]/text()'
-        precautions = '/html/body/div[1]/div/div/div[2]/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[4]/div/div/div/div/p[5]/text()'
-        precautions_2 = '/html/body/div[2]/div/div/div[2]/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[4]/div/div/div/div/p[6]/text()'
-        test_preparation_xp = '/html/body/div[1]/div/div/div[2]/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[5]/div/div/p'
-        understanding_results_xp = '/html/body/div[1]/div/div/div[2]/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[6]/div/div/div/div'
         #
-        understanding_results = tree.xpath(understanding_results_xp)  # find children
-        num = len(understanding_results)
-        understanding_results = []
-        for x in range(num):
-            understanding_results.append(understanding_results_xp + '/p[{}]/text()'.format(x+1))
+        row_elements = [name, alt_name, price]
+        # row = []
+        # for elem in row_elements:
+        #     text = tree.xpath(elem)[0]
+        #     row.append(text)
+        row = [tree.xpath(elem)[0] for elem in row_elements]
+        if row[1] == '':
+            row[1] = row[0]
         #
-        test_preparation = tree.xpath(test_preparation_xp)  # find children
-        num = len(test_preparation)
-        test_preparation = []
-        for x in range(num):
-            test_preparation.append(test_preparation_xp + '/text()[{}]'.format(x+1))
+        soup = BeautifulSoup(res.text, 'lxml')
+        text = soup.text.replace('\n', ' ')
+        what_is_this_test = text[text.find('What is this test?')+len('What is this test?'):text.find('Why this test is performed?')]
+        why_performed = text[text.find('Why this test is performed?')+len('Why this test is performed?'):text.find('Frequency:')]
+        frequency = text[text.find('Frequency:')+len('Frequency:'):text.find('Precautions:')]
+        if not frequency:
+            frequency = text[text.find('Frequency:')+len('Frequency:'):text.find('Test Preparation')]
+        precautions = text[text.find('Precautions:')+len('Precautions:'):text.find('Test Preparation')]
+        test_preparation = text[text.find('Test Preparation')+len('Test Preparation'):text.find('Understanding your test results')]
+        understanding_results = text[text.find('Understanding your test results')+len('Understanding your test results'):text.find('Your Cart')]
         #
-        row_elements = [[name], [alt_name], [price], [what_is_this_test, what_is_this_test_2],
-        [why_performed], [frequency], [precautions, precautions_2], test_preparation, understanding_results]
-        row = []
-        for elem in row_elements:
-            if len(elem) > 1:
-                text = [' '.join(tree.xpath(part)) for part in elem]
-            else:
-                text = tree.xpath(elem[0])
-            text = ' '.join(text)
-            row.append(text)
-        row.append(url)
+        row += [what_is_this_test, why_performed, frequency, precautions, test_preparation, understanding_results, url]
+        print([item[:20] for item in row])
         if not all(row):
-            print('Empty data in url {}'.format(url))
+            print(f'----> Empty data in url {url}\n')
             # continue
         with open(filename, 'a+') as file:
             if url not in done:  # URL not in file
@@ -91,9 +85,11 @@ if 1:
     for city in all_cities:
         folder_name = 'csv_practo'
         print(f'Processing for city {city}')
+        os.system(f'mkdir csv_practo/{city} > /dev/null 2>&1')
         for category in all_categories:
             print(f'    Processing for category {category}', end='')
-            filename = f'{folder_name}/{city}_{category}.csv'
+            filename = f'{folder_name}/{city}/{category}.csv'
+            df = pd.DataFrame(columns=test_columns)  #
             with open(filename, 'a+') as file:
                 file.seek(0, 0)
                 if file.read():  # if not empty, load urls from file
@@ -103,7 +99,7 @@ if 1:
                 else:  # if empty, write first column
                     file.seek(0, 0)
                     csvwriter = csv.writer(file)
-                    csvwriter.writerow(['Test name', 'Alternate name', 'Price', 'What is this test?', 'Why this test is performed?', 'Frequency', 'Precautions', 'Test Preparation', 'Understanding your test results', 'Link'])
+                    csvwriter.writerow(test_columns)
             category_url = category_url_format.format(category, city)
             url = category_url.format(city)
             print('.', end='', flush=True)
@@ -131,17 +127,18 @@ if 1:
             # Tests
             try:
                 process_tests_list(tests_list, filename)
-            except:
+            except KeyboardInterrupt:
+                exit()
+            except Exception:
                 pass
             # break
-        # break  # temp
+        #break  # temp
 
 
-driver.quit()
-
-'''while 1:
+'''
+while 1:
     main()
     break  # temp
     print('Done. Waiting for 5 minutes.')
-    time.sleep(300)'''
-
+    time.sleep(300)
+'''
